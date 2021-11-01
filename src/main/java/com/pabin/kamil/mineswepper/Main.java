adc25849e30178e07bd12afd7610f0fc78250fd2
Kamil Pabin <pabin.kamil@gmail.com>
2021-11-01 19:58:29 +0100

    Generate minefield

--- src/main/java/com/pabin/kamil/mineswepper/Main.java
+++ src/main/java/com/pabin/kamil/mineswepper/Main.java
@@ -1,8 +1,37 @@
 package com.pabin.kamil.mineswepper;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+
 public final class Main {
 
     public static void main(String[] args) {
+        int size = 5;
+        int numberOfMines= 5;
+        int[][] field = new int[5][5];
 
+        SecureRandom generator = new SecureRandom();
+        for(int i = 0; i< numberOfMines; i++) {
+            int x = generator.nextInt(size);
+            int y = generator.nextInt(size);
+            if(field[x][y] == -1) {
+                x = generator.nextInt(size);
+                y = generator.nextInt(size);
+            }
+            field[x][y] = -1;
+            for(int j = x - 1 ; j < x + 2; j++) {
+                for(int k = y - 1 ; k < y + 2; k++) {
+                    try {
+                        if(field[j][k] != -1)
+                        field[j][k]++;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("On boundary");
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < size;i++) {
+            System.out.println(Arrays.toString(field[i]));
+        }
     }
 }
